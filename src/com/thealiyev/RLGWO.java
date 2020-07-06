43063c170d017c740ac235ca3ee852833a5922ff
Royal Aliyev <royalaliyev@Royals-MacBook-Air.local>
2020-07-06 07:29:37 +0300

    Initial commit

---
+++ src/com/thealiyev/RLGWO.java
@@ -0,0 +1,241 @@
+package com.thealiyev;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RLGWO {
+    private static Random random = null;
+
+    public static void main(String[] args) {
+        RLGWO rlgwo = new RLGWO();
+        rlgwo.RLGWO();
+    }
+
+    private void RLGWO() {
+        System.out.println("Reinforcement Learning Gray Wolf Optimization Starts...");
+        random = new Random();
+        System.out.println("Reinforcement Learning Initialization Starts...");
+        ArrayList<ArrayList<Double>> QTable = new ArrayList<>();
+        ArrayList<Double> vector = new ArrayList<>();
+
+        vector.add(0.0);
+        vector.add(0.0);
+        QTable.add(vector);
+        vector = new ArrayList<>();
+
+        vector.add(0.0);
+        vector.add(0.0);
+        QTable.add(vector);
+
+        double QValue, MaxQValue, reward;
+        double alpha = 0.7, gamma = 0.8;
+        System.out.println("Gray Wolf Initialization Starts...");
+        double a;
+        double r1, r2;
+        double A, A1, A2, A3;
+        double C1, C2, C3;
+        double Dalpha, Dbeta, Ddelta;
+        double Xalpha, Xbeta, Xdelta;
+        double X, X1, X2, X3;
+        int population = 30, dimension = 100;
+        double min = -100.0, max = 100.0;
+        int iteration = 500;
+        double sigma1 = 7.0 / 10.0, sigma2 = 2.0 / 10.0, sigma3 = 1.0 / 10.0;
+
+        ArrayList<ArrayList<Double>> optimizationMatrix = createOptimizationMatrix(population, dimension, min, max);
+        ArrayList<Double> fitnessValues = findFitnessValues(optimizationMatrix);
+        ArrayList<Double> sortedFitnessValues = sortFitnessValue(fitnessValues);
+
+        System.out.println("Alpha's Fitness Value at the Initialization: " + sortedFitnessValues.get(0));
+        System.out.println("Alpha's Values at the Initialization: " + optimizationMatrix.get(fitnessValues.indexOf(sortedFitnessValues.get(0))));
+
+        System.out.println("Gray Wolf Iteration Starts...");
+        for (int stCounter = 0; stCounter < iteration; stCounter = stCounter + 1) {
+            a = 2.0 - 2.0 * stCounter / iteration;
+            for (int ndCounter = 0; ndCounter < optimizationMatrix.size(); ndCounter = ndCounter + 1) {
+                for (int rdCounter = 0; rdCounter < optimizationMatrix.get(ndCounter).size(); rdCounter = rdCounter + 1) {
+                    X = optimizationMatrix.get(ndCounter).get(rdCounter);
+                    if (X < min) {
+                        X = min;
+                    } else if (X > max) {
+                        X = max;
+                    }
+
+                    r1 = random.nextDouble();
+                    r2 = random.nextDouble();
+                    A1 = 2 * a * r1 - a;
+                    C1 = 2 * r2;
+                    Xalpha = optimizationMatrix.get(fitnessValues.indexOf(sortedFitnessValues.get(0))).get(rdCounter);
+                    Dalpha = C1 * Xalpha - X;
+                    if (Dalpha < 0) {
+                        Dalpha = Dalpha * -1;
+                    }
+                    X1 = Xalpha - A1 * Dalpha;
+
+                    r1 = random.nextDouble();
+                    r2 = random.nextDouble();
+                    A2 = 2 * a * r1 - a;
+                    C2 = 2 * r2;
+                    Xbeta = optimizationMatrix.get(fitnessValues.indexOf(sortedFitnessValues.get(1))).get(rdCounter);
+                    Dbeta = C2 * Xbeta - X;
+                    if (Dbeta < 0) {
+                        Dbeta = Dbeta * -1;
+                    }
+                    X2 = Xbeta - A2 * Dbeta;
+
+                    r1 = random.nextDouble();
+                    r2 = random.nextDouble();
+                    A3 = 2 * a * r1 - a;
+                    C3 = 2 * r2;
+                    Xdelta = optimizationMatrix.get(fitnessValues.indexOf(sortedFitnessValues.get(2))).get(rdCounter);
+                    Ddelta = C3 * Xdelta - X;
+                    if (Ddelta < 0) {
+                        Ddelta = Ddelta * -1;
+                    }
+                    X3 = Xdelta - A3 * Ddelta;
+
+                    A = (A1 + A2 + A3) / 3;
+                    if (A < 0) {
+                        A = -1.0 * A;
+                    }
+
+                    if (A > 1) {
+                        //State = exploration, work on 1st row of Q Table
+                        if (QTable.get(0).get(0) > QTable.get(0).get(1)) {
+                            //Action = exploration
+                            QValue = QTable.get(0).get(0);
+                            X = (X1 + X2 + X3) / 3;
+                            if (X < optimizationMatrix.get(ndCounter).get(rdCounter)) {
+                                reward = 1.0;
+                            } else {
+                                reward = -1.0;
+                            }
+                            //Finds Q max
+                            MaxQValue = QTable.get(0).get(0);
+                            if (QTable.get(0).get(1) > MaxQValue) {
+                                MaxQValue = QTable.get(0).get(1);
+                            }
+                            //Calculate Q and update Q Table
+                            QValue = QValue + alpha * (reward + gamma * MaxQValue - QValue);
+                            QTable.get(0).set(0, QValue);
+                        } else {
+                            //Action = exploitation
+                            QValue = QTable.get(0).get(1);
+                            X = X1 * sigma1 + X2 * sigma2 + X3 * sigma3;
+                            if (X < optimizationMatrix.get(ndCounter).get(rdCounter)) {
+                                reward = 1.0;
+                            } else {
+                                reward = -1.0;
+                            }
+                            //Finds Q max
+                            MaxQValue = QTable.get(1).get(0);
+                            if (QTable.get(1).get(1) > MaxQValue) {
+                                MaxQValue = QTable.get(1).get(1);
+                            }
+                            //Calculate Q and update Q Table
+                            QValue = QValue + alpha * (reward + gamma * MaxQValue - QValue);
+                            QTable.get(0).set(1, QValue);
+                        }
+                    } else {
+                        //State = exploitation, work on 2nd row of Q Table
+                        if (QTable.get(1).get(0) > QTable.get(1).get(1)) {
+                            //Action = exploration
+                            QValue = QTable.get(1).get(0);
+                            X = (X1 + X2 + X3) / 3;
+                            if (X < optimizationMatrix.get(ndCounter).get(rdCounter)) {
+                                reward = 1.0;
+                            } else {
+                                reward = -1.0;
+                            }
+                            //Finds Q max
+                            MaxQValue = QTable.get(0).get(0);
+                            if (QTable.get(0).get(1) > MaxQValue) {
+                                MaxQValue = QTable.get(0).get(1);
+                            }
+                            //Calculate Q and update Q Table
+                            QValue = QValue + alpha * (reward + gamma * MaxQValue - QValue);
+                            QTable.get(1).set(0, QValue);
+                        } else {
+                            //Action = exploitation
+                            QValue = QTable.get(1).get(1);
+                            X = X1 * sigma1 + X2 * sigma2 + X3 * sigma3;
+                            if (X < optimizationMatrix.get(ndCounter).get(rdCounter)) {
+                                reward = 1.0;
+                            } else {
+                                reward = -1.0;
+                            }
+                            //Finds Q max
+                            MaxQValue = QTable.get(1).get(0);
+                            if (QTable.get(1).get(1) > MaxQValue) {
+                                MaxQValue = QTable.get(1).get(1);
+                            }
+                            //Calculate Q and update Q Table
+                            QValue = QValue + alpha * (reward + gamma * MaxQValue - QValue);
+                            QTable.get(1).set(1, QValue);
+                        }
+                    }
+
+                    optimizationMatrix.get(ndCounter).set(rdCounter, X);
+                }
+            }
+            fitnessValues = findFitnessValues(optimizationMatrix);
+            sortedFitnessValues = sortFitnessValue(fitnessValues);
+            System.out.println(sortedFitnessValues.get(0));
+        }
+        System.out.println("Gray Wolf Iteration Ends...");
+    }
+
+    private ArrayList<ArrayList<Double>> createOptimizationMatrix(int population, int dimension, double min,
+                                                                  double max) {
+        random = new Random();
+        double X;
+
+        ArrayList<ArrayList<Double>> optimizationMatrix = new ArrayList<>();
+        ArrayList<Double> optimizationVector = new ArrayList<>();
+
+        for (int stCounter = 0; stCounter < population; stCounter = stCounter + 1) {
+            for (int ndCounter = 0; ndCounter < dimension; ndCounter = ndCounter + 1) {
+                X = min + (max - min) * random.nextDouble();
+                optimizationVector.add(X);
+            }
+            optimizationMatrix.add(optimizationVector);
+            optimizationVector = new ArrayList<>();
+        }
+
+        return optimizationMatrix;
+    }
+
+    private ArrayList<Double> findFitnessValues(ArrayList<ArrayList<Double>> optimizationMatrix) {
+        ArrayList<Double> fitnessValues = new ArrayList<>();
+
+        double sphere = 0.0;
+        for (int stCounter = 0; stCounter < optimizationMatrix.size(); stCounter = stCounter + 1) {
+            for (int ndCounter = 0; ndCounter < optimizationMatrix.get(stCounter).size(); ndCounter = ndCounter + 1) {
+                sphere = sphere + optimizationMatrix.get(stCounter).get(ndCounter) * optimizationMatrix.get(stCounter).get(ndCounter);
+            }
+            fitnessValues.add(sphere);
+            sphere = 0.0;
+        }
+
+        return fitnessValues;
+    }
+
+    private ArrayList<Double> sortFitnessValue(ArrayList<Double> fitnessValues) {
+        ArrayList<Double> duplicatedFitnessValues = new ArrayList<>(fitnessValues);
+        ArrayList<Double> trio = new ArrayList<>();
+
+        double min;
+        for (int stCounter = 0; stCounter < fitnessValues.size(); stCounter = stCounter + 1) {
+            min = duplicatedFitnessValues.get(0);
+            for (int ndCounter = 0; ndCounter < duplicatedFitnessValues.size(); ndCounter = ndCounter + 1) {
+                if (duplicatedFitnessValues.get(ndCounter) < min) {
+                    min = duplicatedFitnessValues.get(ndCounter);
+                }
+            }
+            trio.add(min);
+            duplicatedFitnessValues.remove(new Double(min));
+        }
+
+        return trio;
+    }
+}
